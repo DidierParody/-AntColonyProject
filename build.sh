b26ee95aa1846d9766eaefabc5b2e92a1b435923
DidierParody <torresparodisdidierjose@gmail.com>
2025-11-01 23:00:20 -0500

    fix: add file listing to build script for debugging

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
+
 # Salir si hay un error
 set -e
+
+# Listar todos los archivos para diagnosticar
+echo "--- Listando archivos del proyecto ---"
+ls -R
+echo "------------------------------------"
 
 # Construir el cliente de React
 cd client
